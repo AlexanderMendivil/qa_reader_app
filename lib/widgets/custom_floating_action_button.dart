6e4ac229e3c631ae34ad68b8760a1ace2e957633
Alexander Mendivil <alexanderima@outlook.es>
2023-03-24 19:57:07 -0700

    barcode

--- lib/widgets/custom_floating_action_button.dart
+++ lib/widgets/custom_floating_action_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
   const CustomFloatingActionButton({
@@ -8,7 +9,9 @@ class CustomFloatingActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode( '#3D8BEF', 'CANCEL', false, ScanMode.QR);
+      },
       child:  const Icon(Icons.filter_center_focus),);
   }
 }
