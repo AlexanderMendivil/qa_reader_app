4e6be355db9a3db3001b9e3459f37c507281096b
Alexander Mendivil <alexanderima@outlook.es>
2023-04-03 20:44:44 -0700

    google maps integration

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,19 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../models/scan_model.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
    
   const MapPage({Key? key}) : super(key: key);
-  
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+
+  Completer<GoogleMapController> _controller = Completer();
   @override
   Widget build(BuildContext context) {
 
+
+    final CameraPosition _initPoint = CameraPosition(target: LatLng(37, -122), zoom: 14);
     final scan = ModalRoute.of(context)?.settings.arguments as ScanModel;
     return Scaffold(
       appBar: AppBar( title: const Text('Coordenadas'),),
-      body: Center(
-         child: Text(scan.valor),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _initPoint,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
